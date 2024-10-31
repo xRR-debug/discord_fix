9de4d1e4c10e808cf2b6b16a3921016af81339cd
шта <68876628+xRR-debug@users.noreply.github.com>
2024-10-31 18:41:28 +0300

    update 30.10.24

--- service_install_discord_youtube.bat
+++ service_install_discord_youtube.bat
@@ -1,7 +1,8 @@
-set ARGS=--wf-tcp=443-65535 --wf-udp=443-65535 ^
+set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="%~dp0list-discord_youtube.txt" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
---filter-tcp=443 --hostlist="%~dp0list-discord_youtube.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%~dp0tls_clienthello_www_google_com.bin"
+--filter-tcp=80 --hostlist="%~dp0list-discord_youtube.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist="%~dp0list-discord_youtube.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --dpi-desync-fake-tls="%~dp0quic_initial_www_google_com.bin"
 
 set SRVCNAME=zapret (discord)
 
